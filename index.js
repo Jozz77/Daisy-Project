df190dcb9cdf8872b619956a6773be48c818a45f
Jozz77 <mmadujonathan@gmail.com>
2023-02-04 16:38:53 +0100

    Feature: add a scroll-to-up button and its animation

---
+++ index.js
@@ -0,0 +1,6 @@
+// let arrowUp = document.getElementsByClassName("arrow");
+
+document.addEventListener("click", function(){
+    let arrowUp = document.getElementsByClassName("arrow");
+    arrowUp = document.documentElement.scrollTop = 0;
+  });
